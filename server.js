f6e2e3eb8d8ce460b93984d758bf99f30ed741b5
ZainUlBasit <zainulbasit486@gmail.com>
2024-09-02 16:31:13 +0500

    changes

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://appspot.com.pk",
+  "https://www.appspot.com.pk",
   "https://www.appspotsoftwares.com",
   "https://app-spot-admin.vercel.app",
 ];
